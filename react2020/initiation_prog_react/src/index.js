012bacca33c73c570205ccf43927c8e25a7edce0
Enelith <jonathan.vinh@gmail.com>
2021-03-08 15:42:06 +0100

    Maj Section 12 - Le JSX

--- react2020/initiation_prog_react/src/index.js
+++ react2020/initiation_prog_react/src/index.js
@@ -1,4 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render("coucou", document.getElementById("root"));
+// Le Code JSX est très proche du HTML
+// ReactDOM.render(CODE_JSX, Target Element from DOM);
+ReactDOM.render(<h1 className="test">Hello World</h1>, document.getElementById("root"));
+// <=> ReactDOM.render(React.createElement("h1", null, "Hello World"), document.getElementById("root"));
