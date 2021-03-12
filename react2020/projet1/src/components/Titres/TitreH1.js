324132e4086f0dc2b3b7b1ad5fa2fa0783139de8
Enelith <jonathan.vinh@gmail.com>
2021-03-12 17:42:36 +0100

    Maj Section 042 - Changer la police

--- react2020/projet1/src/components/Titres/TitreH1.js
+++ react2020/projet1/src/components/Titres/TitreH1.js
@@ -1,19 +1,16 @@
 import React from 'react';
 
-/*
+import classes from './TitreH1.module.css';
+
 // A utiliser si on a besoin de faire des actions avant de renvoyer le rendu (ex : placer du JS, logique, ...)
 const titreH1 = (props) => {
     // Placer du JS ici si besoin
+    const monCss = `${classes.policeTitre} border border-dark p-2 mt-2 text-center bg-primary rounded`;
 
     // Resultat final incluant le JS
     return (
-        
+        <h1 className={monCss}>{props.children}</h1>
     );
 };
-*/
-
-const titreH1 = (props) => (
-    <h1 className="border border-dark p-2 mt-2 text-white text-center bg-primary rounded">{props.children}</h1>
-);
 
 export default titreH1;
