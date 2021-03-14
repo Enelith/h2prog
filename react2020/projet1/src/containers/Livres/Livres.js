e31f18fe7c25f386e821ef09e5f3ebf95bbb678e
Enelith <jonathan.vinh@gmail.com>
2021-03-14 17:19:43 +0100

    Maj Section 045 - Liste de Livres

--- react2020/projet1/src/containers/Livres/Livres.js
+++ react2020/projet1/src/containers/Livres/Livres.js
@@ -7,6 +7,15 @@ import classes from './Livres.module.css';
 */
 
 class Livres extends Component {
+    state = {
+        livres: [
+            { id: 1, titre: "Bouquin 1", auteur: "Alpha", nbPages: 200 },
+            { id: 2, titre: "Bouquin 2", auteur: "Beta", nbPages: 52 },
+            { id: 3, titre: "Bouquin 3", auteur: "Gamma", nbPages: 353 },
+            { id: 4, titre: "Bouquin 4", auteur: "Kappa", nbPages: 120 },
+        ]
+    }
+
     render() {
         return (
             <>
@@ -20,21 +29,27 @@ class Livres extends Component {
                         </tr>
                     </thead>
                     <tbody>
-                        <tr>
-                            <td>titre1</td>
-                            <td>auteur1</td>
-                            <td>X pages</td>
-                            <td>
-                                <Bouton typeBtn="btn-warning" buttonAction={() => console.log("Modifier")}>
-                                    Modification
-                                </Bouton>
-                            </td>
-                            <td>
-                                <Bouton typeBtn="btn-danger" buttonAction={() => console.log("Supprimer")}>
-                                    Supprimer
-                                </Bouton>
-                            </td>
-                        </tr>
+                        {
+                            this.state.livres.map(livre => {
+                                return (
+                                    <tr key={ livre.id }>
+                                        <td>{ livre.titre }</td>
+                                        <td>{ livre.auteur }</td>
+                                        <td>{ livre.nbPages }</td>
+                                        <td>
+                                            <Bouton typeBtn="btn-warning" buttonAction={() => console.log("Modifier")}>
+                                                Modification
+                                            </Bouton>
+                                        </td>
+                                        <td>
+                                            <Bouton typeBtn="btn-danger" buttonAction={() => console.log("Supprimer")}>
+                                                Supprimer
+                                            </Bouton>
+                                        </td>
+                                    </tr>
+                                );
+                            })
+                        }
                     </tbody>
                 </table>
             </>
