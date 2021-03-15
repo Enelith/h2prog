994b9f7bbeae823df2f228209eded786734f7afa
Enelith <jonathan.vinh@gmail.com>
2021-03-15 17:14:15 +0100

    Fix class => className (JSX)

--- react2020/projet1/src/components/Alert/Alert.js
+++ react2020/projet1/src/components/Alert/Alert.js
@@ -3,7 +3,7 @@ import React from 'react';
 const alert = (props) => {
     const myCss = `alert ${props.typeAlert}`;
     return (
-        <div class={ myCss } role="alert">
+        <div className={ myCss } role="alert">
             {props.children}
         </div>
     );
