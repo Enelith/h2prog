447ad3880f76d2836f63064d117c8cb05745419c
Enelith <jonathan.vinh@gmail.com>
2021-03-23 15:41:03 +0100

    Maj Section 082 - La pagination

--- react2020/projet3/src/containers/PaysManager/PaysManager.js
+++ react2020/projet3/src/containers/PaysManager/PaysManager.js
@@ -11,7 +11,8 @@ class PaysManager extends Component {
     state = {
         listePays: [],
         loading: false,
-        regionSelected: null
+        regionSelected: null,
+        currentPage: 1
     }
 
     componentDidMount = () => {
@@ -46,62 +47,95 @@ class PaysManager extends Component {
                 this.setState({
                     listePays,
                     loading: false,
-                    regionSelected: region
+                    regionSelected: region,
+                    currentPage: 1
                 }); // <=> this.setState({listePays: listePays});
             })
             .catch(error => {
                 this.setState({
                     loading: false,
-                    regionSelected: region
+                    regionSelected: region,
+                    currentPage: 1
                 });
             });
     }
 
     render() {
+        let pagination = [];
+        let displayListePays = [];
+
+        if (this.state.listePays) {
+            let nbBoutonsRequis = this.state.listePays.length / 10; // Max affichage 10 elements
+
+            if (this.state.listePays.length % 10 !== 0) {
+                nbBoutonsRequis++;
+            }
+
+            for (let i = 1; i <= nbBoutonsRequis; i++) {
+                pagination.push(
+                    <Bouton key={i}
+                        typeBtn="btn-info"
+                        buttonAction={() => this.setState({ currentPage: i })}
+                        isSelected={this.state.currentPage === i}
+                    >{i}</Bouton>
+                );
+            }
+
+            const startIndex = (this.state.currentPage - 1) * 10;
+            const endIndex = this.state.currentPage * 10;
+            const listeReduite = this.state.listePays.slice(startIndex, endIndex);
+
+            displayListePays = listeReduite.map(pays => {
+                return (
+                    <div key={pays.code} className="col-12 col-md-6 "><Pays {...pays} /></div>
+                );
+            })
+        }
+
         return (
             <>
                 <div className="container">
                     <Titre >Liste des pays du monde</Titre>
 
-                    <Bouton typeBtn="btn-info"
-                        buttonAction={() => this.changeRegionHandler('all')}
-                        isSelected={this.state.regionSelected === 'all'} > Tous</Bouton>
+                    <div className="row no-gutters">
+                        <div className="col-lg-8 col-md-12">
+                            <Bouton typeBtn="btn-info"
+                                buttonAction={() => this.changeRegionHandler('all')}
+                                isSelected={this.state.regionSelected === 'all'} > Tous</Bouton>
 
-                    <Bouton typeBtn="btn-info"
-                        buttonAction={() => this.changeRegionHandler('europe')}
-                        isSelected={this.state.regionSelected === 'europe'} > Europe</Bouton>
+                            <Bouton typeBtn="btn-info"
+                                buttonAction={() => this.changeRegionHandler('europe')}
+                                isSelected={this.state.regionSelected === 'europe'} > Europe</Bouton>
 
-                    <Bouton typeBtn="btn-info"
-                        buttonAction={() => this.changeRegionHandler('africa')}
-                        isSelected={this.state.regionSelected === 'africa'} >Afrique</Bouton>
+                            <Bouton typeBtn="btn-info"
+                                buttonAction={() => this.changeRegionHandler('africa')}
+                                isSelected={this.state.regionSelected === 'africa'} >Afrique</Bouton>
 
-                    <Bouton typeBtn="btn-info"
-                        buttonAction={() => this.changeRegionHandler('asia')}
-                        isSelected={this.state.regionSelected === 'asia'} >Asie</Bouton>
+                            <Bouton typeBtn="btn-info"
+                                buttonAction={() => this.changeRegionHandler('asia')}
+                                isSelected={this.state.regionSelected === 'asia'} >Asie</Bouton>
 
-                    <Bouton typeBtn="btn-info"
-                        buttonAction={() => this.changeRegionHandler('americas')}
-                        isSelected={this.state.regionSelected === 'americas'} >Am&eacute;rique</Bouton>
+                            <Bouton typeBtn="btn-info"
+                                buttonAction={() => this.changeRegionHandler('americas')}
+                                isSelected={this.state.regionSelected === 'americas'} >Am&eacute;rique</Bouton>
 
-                    <Bouton typeBtn="btn-info"
-                        buttonAction={() => this.changeRegionHandler('oceania')}
-                        isSelected={this.state.regionSelected === 'oceania'} >Oc&eacute;anie</Bouton>
-
+                            <Bouton typeBtn="btn-info"
+                                buttonAction={() => this.changeRegionHandler('oceania')}
+                                isSelected={this.state.regionSelected === 'oceania'} >Oc&eacute;anie</Bouton>
+                        </div>
+                        <div className="col-lg-4 text-right">
+                            Nombre de pays : <span className="badge badge-success">{this.state.listePays.length}</span>
+                        </div>
+                    </div>
                     {
                         this.state.loading ?
                             <div>Loading...</div> :
                             <div className="row no-gutters">
-                                {
-                                    this.state.listePays.map(pays => {
-                                        return (
-                                            <div key={pays.code} className="col-12 col-md-6 "><Pays {...pays} /></div>
-                                        );
-                                    })
-                                }
+                                {displayListePays}
                             </div>
                     }
 
-                    <div>Pagination</div>
+                    <div>{pagination}</div>
                 </div>
             </>
         );
