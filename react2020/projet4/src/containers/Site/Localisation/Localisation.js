a11c62f575facd084f19ea3393516af415acfd88
Enelith <jonathan.vinh@gmail.com>
2021-03-26 18:46:21 +0100

    Maj Section 099 - Page localisation (Step 1)

--- react2020/projet4/src/containers/Site/Localisation/Localisation.js
+++ react2020/projet4/src/containers/Site/Localisation/Localisation.js
@@ -1,10 +1,89 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+
+
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+import Titre from '../../../components/UI/Titres/TitreH1';
+import Button from 'react-bootstrap/Button';
+
+import Select from 'react-select';
 
 class Localisation extends Component {
+    state = {
+        loading: false,
+        departements: [],
+        departementSelected: null,
+        recherche: null
+    }
+
+    componentDidMount = () => {
+        this.setState({ loading: true });
+
+        axios.get(process.env.REACT_APP_API_GOUV_FR_DEPARTEMENTS)
+            .then(response => {
+                const departements = response.data.map(departement => {
+                    return {
+                        label: departement.nom,
+                        value: departement.code
+                    }
+                });
+                this.setState({ departements, departementSelected: departements[0].value, loading: false });
+            })
+            .catch(error => {
+                console.log(error);
+                this.setState({ loading: false });
+            })
+    }
+
+    rechercheEtablissement = (type) => {
+        console.log(process.env.REACT_APP_API_GOUV_FR_TYPE_BY_DEPARTEMENT + `${this.state.departementSelected}/${type}`);
+        axios.get(process.env.REACT_APP_API_GOUV_FR_TYPE_BY_DEPARTEMENT + `${this.state.departementSelected}/${ type }`)
+            .then(response => {
+                console.log(response.data);
+                this.setState({
+                    recherche: response.data
+                })
+            })
+    }
+
+    /*
+     * CF API https://api.gouv.fr/documentation/api_etablissements_publics
+     */
+
     render() {
         return (
             <>
-Localisation
+                <Titre>Rechercher un &eacute;tablissement</Titre>
+
+                {
+                    this.state.loading && <div>Chargement...</div>
+                }
+                {
+                    !this.state.loading && this.state.departements.length === 0 && <div>Api non disponible</div>
+                }
+                {
+                    !this.state.loading && this.state.departements.length > 0 && 
+                    <div>
+                        <Row>
+                            <Col sm={4}>
+                                <Select
+                                    options={this.state.departements}
+                                    defaultValue={this.state.departements[0]}
+                                    onChange={(target) => this.setState({ departementSelect: target.code })} />
+                            </Col>
+                            <Col sm={8}>
+                                <Button variant="secondary" onClick={() => this.rechercheEtablissement('mairie')} >Mairie</Button>&nbsp;
+                                <Button variant="secondary" onClick={() => this.rechercheEtablissement('commissariat_police')} >Commissariat de police</Button>&nbsp;
+                                <Button variant="secondary" onClick={() => this.rechercheEtablissement('pole_emploi')} >P&ocirc;le emploi</Button>&nbsp;
+                                <Button variant="secondary" onClick={() => this.rechercheEtablissement('prefecture')} >Pr&eacute;fecture</Button>
+                            </Col>
+                        </Row>
+                    </div>
+                }
+                
+
             </>
         );
     }
