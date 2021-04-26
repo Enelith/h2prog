a0c321686ba042c050a8fdbdf722bccec88a90b1
Enelith <jonathan.vinh@gmail.com>
2021-04-26 18:22:18 +0200

    Maj Part 20 - L'exemple en PHP

---
+++ poo_algo_bases_indispensables/Section 09/Part 20/index.php
@@ -0,0 +1,31 @@
+<?php
+
+class Personnage {
+	private $nom;
+	private $age;
+	private $sexe;
+
+	public function __construct($nom, $age, $sexe) {
+		$this->nom = $nom;
+		$this->age = $age;
+		$this->sexe = $sexe;
+	}
+
+	public function getNom() { return $this->nom; }
+	public function setNom($nom) { $this->nom = $nom; }
+	
+	public function getAge() { return $this->age; }
+	public function setAge($age) { $this->age = $age; }
+	
+	public function getSexe() { return $this->sexe; }
+	public function setSexe($sexe) { $this->sexe = $sexe; }
+
+	public function sayHello() {
+		echo "Bonjour, c'est moi " . $this->nom;
+	}
+}
+
+$perso = new Personnage("Tya", 22, false);
+$perso->sayHello();
+
+?>
