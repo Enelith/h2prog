28ed29114461d08162c4e665954d7d6ae8ad5db1
Enelith <jonathan.vinh@gmail.com>
2021-06-10 14:29:10 +0200

    Maj Part 108 - Lister les formations

--- react2020/projet5/src/containers/Formations/Formations.js
+++ react2020/projet5/src/containers/Formations/Formations.js
@@ -1,7 +1,21 @@
 import TitreH1 from "components/TitreH1/TitreH1";
 import Bouton from "components/Bouton/Bouton";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function Formations() {
+    const [formations, setFormations] = useState(null);
+
+    useEffect(() => {
+        axios.get(process.env.REACT_APP_H2PROG_FORMATIONS)
+            .then(response => {
+                setFormations(response.data);
+            })
+            .catch(error => {
+                console.log(error);
+            })
+    }, []);
+
     return (
         <>
             <TitreH1>Bienvenu sur le site listant les formations H2PROG</TitreH1>
@@ -9,6 +23,33 @@ function Formations() {
             <Bouton>PHP</Bouton>
             <Bouton>Algorithmique</Bouton>
             <Bouton>JavaScript</Bouton>
+
+            <table className="table">
+                <thead>
+                    <th>Num&eacute;ro</th>
+                    <th>Libell&eacute;</th>
+                    <th>Description</th>
+                    <th>Cat&eacute;gorie</th>
+                    <th>Image</th>
+                </thead>
+                <tbody>
+                    {
+                        formations && formations.map(formation => {
+                            return (
+                                <tr key={formation.id}>
+                                    <td>{formation.id}</td>
+                                    <td>{formation.libelle}</td>
+                                    <td>{formation.description}</td>
+                                    <td>{formation.categorie}</td>
+                                    <td>
+                                        <img src={formation.image} alt={formation.image} width="100px"/>
+                                    </td>
+                                </tr>
+                            );
+                        })
+                    }
+                </tbody>
+            </table>
         </>
     );
 }
